383ac98bf7c7058bdbc800acef9efb586a12efac
Yegor Bugayenko <yegor256@gmail.com>
2025-02-23 08:00:46 +0300

    MIT removed

--- src/it/larger/verify.groovy
+++ src/it/larger/verify.groovy
@@ -1,6 +1,4 @@
 /*
- * MIT License
- *
  * SPDX-FileCopyrightText: Copyright (c) 2024-2025 Objectionary.com
  * SPDX-License-Identifier: MIT
  */
--- src/main/java/org/eolang/hone/Mktemp.java
+++ src/main/java/org/eolang/hone/Mktemp.java
@@ -1,6 +1,4 @@
 /*
- * The MIT License (MIT)
- *
  * SPDX-FileCopyrightText: Copyright (c) 2024-2025 Objectionary.com
  * SPDX-License-Identifier: MIT
  */

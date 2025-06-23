f520255f0829404a09234157bd5db87eec338e44
Yegor Bugayenko <yegor256@gmail.com>
2025-06-23 04:11:17 +0300

    #173: Refactor script to improve code readability.

--- src/main/resources/org/eolang/hone/scaffolding/normalize.sh
+++ src/main/resources/org/eolang/hone/scaffolding/normalize.sh
@@ -19,9 +19,9 @@ mkdir -p "${xmir}"
 
 while IFS= read -r f; do
   mkdir -p "$(dirname "${to}/${f}")"
+  mkdir -p "$(dirname "${xmir}/${f}")"
   for rule in ${rules}; do
     phino rewrite --rule "${rule}" < "${from}/${f}" > "${to}/${f}"
-    mkdir -p "$(dirname "${xmir}/${f}")"
     phino rewrite --nothing --output=xmir < "${to}/${f}" > "${xmir}/${f}"
   done
 done < <(find "$(realpath "${from}")" -name '*.phi' -type f -exec realpath --relative-to="${from}" {} \;)

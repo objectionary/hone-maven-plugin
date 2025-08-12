338750a6279a8c576bcc6eab91645c2bee7b8cfd
Yegor Bugayenko <yegor256@gmail.com>
2025-08-12 23:00:31 +0300

    #259: Refactor Docker build command in BuildMojo.java.

--- src/main/java/org/eolang/hone/BuildMojo.java
+++ src/main/java/org/eolang/hone/BuildMojo.java
@@ -73,7 +73,7 @@ public final class BuildMojo extends AbstractMojo {
                         (Scalar<Object>) () -> new Docker(this.sudo).exec(
                             "buildx",
                             "build",
-                            "--output", "type=docker",
+                            "--load",
                             "--pull",
                             "--progress=plain",
                             "--build-arg", String.format("PHINO_VERSION=%s", this.phino()),

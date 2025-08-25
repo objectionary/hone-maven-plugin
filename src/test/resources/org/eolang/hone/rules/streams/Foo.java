ac61a3b2a0773fd65c8b653bdb2caef2d15ab8b8
maxonfjvipon <mtrunnikov@gmail.com>
2025-08-25 15:47:37 +0300

    bug(#296): streams with this

--- src/test/resources/org/eolang/hone/rules/streams/Foo.java
+++ src/test/resources/org/eolang/hone/rules/streams/Foo.java
@@ -5,28 +5,103 @@
 
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class Foo implements Supplier<Long> {
 
     public static final int N = 100_000_000;
 
-    private int z = 5;
+    private static final long[] VALS = IntStream.range(0, N).mapToLong(i -> i % 1000).toArray();
+
+    private final int z = 5;
 
     public static void main(String[] args) {
-        long y = new Foo().get();
-        System.out.printf("%d\n", y);
+        final int[] input = { 1, 2, 4, 8, 16 };
+        final long r = IntStream.of(input)
+            .map(x -> x + 1)
+            .map(Foo::acceptsDouble)
+            .boxed()
+            .filter(Foo::filterBool)
+            .filter(Foo::filterBoolean)
+            .mapToDouble(x -> x)
+            .filter(x -> x > 1)
+            .boxed()
+            .filter(Foo::filterPrim)
+            .map(Foo::acceptsDouble)
+            .map(String::valueOf)
+            .filter(Foo::filterObject)
+            .map(Integer::valueOf)
+            .map(Foo::acceptsDouble)
+            .filter(Foo::filterBoolean)
+            .filter(Foo::filterBool)
+            .map(x -> x.shortValue())
+            .map(Foo::bothPrimitives)
+            .map(Double::longValue)
+            .filter(x -> x > 5)
+            .filter(Foo::filterPrim)
+            .map(Foo::returnsLong)
+            .mapToLong(Foo::mapToLong)
+            .sum();
+        long x = LongStream.of(VALS).map(d -> d + d).sum();
+        long w = new Foo().get();
+        System.out.printf("%d\n", r + x + w);
     }
-    
+
+    private static Integer mapToLong(double x) {
+        return Double.valueOf(x).intValue();
+    }
+
+    private static long returnsLong(long x) {
+        return x;
+    }
+
+    private static Integer acceptsDouble(double x) {
+        return Double.valueOf(x).intValue();
+    }
+
+    private static boolean filterBool(Integer x) {
+        return x > 1;
+    }
+
+    private static Boolean filterBoolean(long x) {
+        return x > 1;
+    }
+
+    private static double bothPrimitives(short x) {
+        return (double) x;
+    }
+
+    private static boolean filterPrim(double x) {
+        return true;
+    }
+
+    private static boolean filterObject(Object x) {
+        return true;
+    }
+
     @Override
     public Long get() {
-        int w = this.func(5);
-        final long r = IntStream.of(1)
+        final int[] input = { 1, 2, 4, 8, 16 };
+        final long r = IntStream.of(input)
+            .map(x -> x + 1)
             .map(this::func)
+            .boxed()
+            .filter(Foo::filterBool)
+            .map(x -> this.func(this.z) + x)
+            .filter(this::filter)
+            .mapToDouble(x -> x)
+            .filter(x -> this.z + x > 2)
+            .boxed()
+            .mapToLong(Foo::mapToLong)
             .sum();
         return r;
     }
 
-    private int func(int num) {
-        return this.z + num;
+    public int func(int x) {
+        return this.z + x;
+    }
+
+    public boolean filter(int x) {
+        return x > 1;
     }
 }

03e577b0621a4ecd38a6cf3ff709c546250da570
Rultor.com <gpg@rultor.com>
2025-08-26 19:30:10 +0000

    0.14.0

--- src/main/java/org/eolang/hone/AbstractMojo.java
+++ src/main/java/org/eolang/hone/AbstractMojo.java
@@ -48,7 +48,7 @@ abstract class AbstractMojo extends org.apache.maven.plugin.AbstractMojo {
      *
      * @since 0.1.0
      */
-    @Parameter(property = "hone.image", defaultValue = "yegor256/hone:latest")
+    @Parameter(property = "hone.image", defaultValue = "yegor256/hone:0.14.0")
     protected String image;
 
     /**

9c4db96175131b914fac9134956a84f27bb2d093
Yegor Bugayenko <yegor256@gmail.com>
2026-01-01 12:41:26 +0300

    year up to 2026

--- .github/benchmark.sh
+++ .github/benchmark.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# SPDX-FileCopyrightText: Copyright (c) 2024-2025 Objectionary.com
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026 Objectionary.com
 # SPDX-License-Identifier: MIT
 
 set -e -o pipefail

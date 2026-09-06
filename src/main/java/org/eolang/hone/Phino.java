e8cf6b4947b23368c83ae84b6239ddfbe01c1945
Vasilev Stas <33578155+VasilevNStas@users.noreply.github.com>
2026-09-06 09:10:51 +0300

    fix(#839): bound the Docker and phino process waits with timeouts (#861)

--- src/main/java/org/eolang/hone/Phino.java
+++ src/main/java/org/eolang/hone/Phino.java
@@ -5,9 +5,11 @@
 package org.eolang.hone;
 
 import com.jcabi.log.Logger;
-import com.yegor256.Jaxec;
-import com.yegor256.Result;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
 
 /**
  * An abstraction of Phino in command line.
@@ -43,28 +45,23 @@ final class Phino {
     boolean available(final String expected) {
         boolean available = false;
         try {
-            final Result result = new Jaxec(this.executable, "--version")
-                .withCheck(false).execUnsafe();
-            if (result.code() == 0) {
-                final String version = result.stdout().trim();
-                if (version.equals(expected)) {
-                    available = true;
-                    Logger.info(
-                        this,
-                        "The 'phino' executable found (%s), no need to use Docker",
-                        version
-                    );
-                } else {
-                    Logger.info(
-                        this,
-                        "The 'phino' executable is found, but its version (%s) is not equal to the expected one (%s); you can upgrade it via 'cabal update && cabal install --overwrite-policy=always phino-%s' (see https://github.com/objectionary/phino for details)",
-                        version, expected, expected
-                    );
-                }
+            final Process proc = new ProcessBuilder(
+                this.executable, "--version"
+            ).start();
+            final ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+            final Thread pump = new Thread(
+                () -> Phino.pump(stdout, proc.getInputStream())
+            );
+            pump.start();
+            final boolean probed = proc.waitFor(3L, TimeUnit.SECONDS);
+            if (probed) {
+                pump.join();
+                available = Phino.version(proc, stdout, expected, this);
             } else {
+                proc.destroyForcibly();
                 Logger.info(
                     this,
-                    "The 'phino' executable is found, but it doesn't work, we must use Docker"
+                    "The 'phino --version' probe timed out, we must use Docker"
                 );
             }
         } catch (final IOException ex) {
@@ -73,7 +70,63 @@ final class Phino {
                 "The 'phino' executable not found, we must use Docker: %s",
                 ex.getMessage()
             );
+        } catch (final InterruptedException ex) {
+            Thread.currentThread().interrupt();
         }
         return available;
+    }
+
+    /**
+     * Drain a process stdout into a buffer, byte by byte.
+     * @param stdout Where to collect the output
+     * @param input The input stream to drain
+     */
+    private static void pump(final ByteArrayOutputStream stdout, final InputStream input) {
+        try {
+            final byte[] buffer = new byte[1024];
+            while (true) {
+                final int read = input.read(buffer);
+                if (read == -1) {
+                    break;
+                }
+                stdout.write(buffer, 0, read);
+            }
+        } catch (final IOException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+
+    /**
+     * Decide whether the probed phino is the expected one.
+     * @param proc The probed process
+     * @param stdout Collected version output
+     * @param expected The version we need
+     * @param self Logger target
+     * @return TRUE if the versions match
+     */
+    private static boolean version(
+        final Process proc, final ByteArrayOutputStream stdout,
+        final String expected, final Phino self
+    ) {
+        final boolean match;
+        final String version = new String(
+            stdout.toByteArray(), StandardCharsets.UTF_8
+        ).trim();
+        if (proc.exitValue() == 0 && version.equals(expected)) {
+            Logger.info(
+                self,
+                "The 'phino' executable found (%s), no need to use Docker",
+                version
+            );
+            match = true;
+        } else {
+            Logger.info(
+                self,
+                "The 'phino' executable is found, but its version (%s) is not equal to the expected one (%s); you can upgrade it via 'cabal update && cabal install --overwrite-policy=always phino-%s' (see https://github.com/objectionary/phino for details)",
+                version, expected, expected
+            );
+            match = false;
+        }
+        return match;
     }
 }

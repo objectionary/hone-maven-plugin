8de8665e0983fdba1918f5da2f575bb172bbdec0
VasilevNStas <ctac@outlook.com>
2026-09-06 10:53:42 +0300

    fix(#864): make CSV.count() tolerate malformed cells

--- src/main/java/org/eolang/hone/OptimizeMojo.java
+++ src/main/java/org/eolang/hone/OptimizeMojo.java
@@ -405,7 +405,7 @@ public final class OptimizeMojo extends AbstractMojo {
             Logger.info(
                 this,
                 "Optimized %d/%d files",
-                csv.count("Changed", v -> Integer.parseInt(v) > 0),
+                csv.count("Changed", CSV::positive),
                 csv.size()
             );
         }
